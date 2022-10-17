96234752dcba29c0c9d1221d6683d1802d59e970
jamesrobertojunglos <jamesrobertojunglos75@gmail.com>
2022-10-17 15:28:59 -0300

    repasar o projeto e ver a parte das views

--- app/Models/Saude.php
+++ app/Models/Saude.php
@@ -49,15 +49,78 @@ function classificacao_imc($imc) {
         return "Obesidade III";
     }
 }
-    public function imc() {
 
-    $valores[] = "";    
-    $valores["nome"] = $_GET["nome"];
-    $valores["idade"]= $this->calcula_idade($_GET["datanascimento"]);
-    $valores["peso"] = $_GET["peso"];
-    $valores["altura"] = $_GET["altura"];
-    $valores["imc"] = $this->calcula_imc($valores["peso"],$valores["altura"]);
-    $valores["classificacaoimc"] = $this->classificacao_imc($valores["imc"]);
-    return $valores;
+
+function classificacao_sono($idade, $horas) {
+    if ($idade <= 0.03) {
+        if ($horas >= 14 && $horas <=17) {
+            return "Horas de sono suficientes";
+        } else {
+            return "Horas de sono insuficientes";
+        }
+    } elseif ($idade >= 0.04 && $idade <= 0.11) {
+        if ($horas >= 12 && $horas <= 15) {
+            return "Horas de sono suficientes";
+        } else {
+            return "Horas de sono insuficientes";
+        }
+    } elseif ($idade >= 1 && $idade <= 2) {
+        if ($horas >= 11 && $horas <= 14) {
+            return "Horas de sono suficientes";
+        } else {
+            return "Horas de sono insuficientes";
+        }
+    } elseif ($idade >= 3 && $idade <= 5) {
+        if ($horas >= 10 && $horas <= 13) {
+            return "Horas de sono suficientes";
+        } else {
+            return "Horas de sono insuficientes";
+        }
+    } elseif ($idade >= 6 && $idade <= 13) {
+        if ($horas >= 9 && $horas <= 11) {
+            return "Horas de sono suficientes";
+        } else {
+            return "Horas de sono insuficientes";
+        }
+    } elseif ($idade >= 14 && $idade <= 17) {
+        if ($horas >= 8 && $horas <= 10) {
+            return "Horas de sono suficientes";
+        } else {
+            return "Horas de sono insuficientes";
+        }
+    } elseif ($idade >= 18 && $idade <= 64) {
+        if ($horas >= 7 && $horas <= 9) {
+            return "Horas de sono suficientes";
+        } else {
+            return "Horas de sono insuficientes";
+        }
+    } elseif ($idade >= 65) {
+        if ($horas >= 7 && $horas <= 8) {
+            return "Horas de sono suficientes";
+        } else {
+            return "Horas de sono insuficientes";
+        }
+    }
 }
-}
+
+    public function imc() {
+   
+        $valores["nome"] = $_GET["nome"];
+        $valores["idade"]= $this->calcula_idade($_GET["datanascimento"]);
+        $valores["peso"] = $_GET["peso"];
+        $valores["altura"] = $_GET["altura"];
+        $valores["imc"] = $this->calcula_imc($valores["peso"],$valores["altura"]);
+        $valores["classificacaoimc"] = $this->classificacao_imc($valores["imc"]);
+        return $valores;
+    }
+
+    public function sono() {
+        $valores["nome"] = $_GET["nome"];
+        $valores["idade"] = $_GET["idade"];
+        $valores["horas"] = $_GET["horas"];
+        $valores["classificacaosono"] = $this->classificacao_sono
+        ($_GET["idade"], $_GET["horas"]);
+        return $valores;
+    }
+
+    }
